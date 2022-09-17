a8765138cdbca7deaf0ca1b5aada5441b9f5286a
Hugo Falcao <hugocoelhof03@gmail.com>
2022-09-17 16:25:01 -0300

    feat: :sparkles: Listagem dos games e anúncios finalizada

--- App.tsx
+++ App.tsx
@@ -6,9 +6,10 @@ import {
   Inter_700Bold,
   Inter_900Black,
 } from "@expo-google-fonts/inter";
-import { Background } from "./src/components/Background";
-import { Home } from "./src/screens/Home";
+
+import { Routes } from "./src/routes";
 import { Loading } from "./src/components/Loading";
+import { Background } from "./src/components/Background";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +26,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Home /> : <Loading />}
+      { fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
